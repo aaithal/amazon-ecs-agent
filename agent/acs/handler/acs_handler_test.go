ac22e8a87410f09d1109dcd7fdbcd5367d02cbcd
Peng Yin <penyin@amazon.com>
2016-12-15 21:34:26 +0000

    Enable concurrent pulling for docker >= 1.9.0
    (cherry picked from commit 5c781800db72f99e84b586cc48625a13e64ccc33)

--- agent/acs/handler/acs_handler_test.go
+++ agent/acs/handler/acs_handler_test.go
@@ -154,7 +154,7 @@ func TestAcsWsUrl(t *testing.T) {
 	if parsed.Query().Get("agentHash") != version.GitHashString() {
 		t.Fatal("Wrong cluster")
 	}
-	if parsed.Query().Get("dockerVersion") != "Docker version result" {
+	if parsed.Query().Get("dockerVersion") != "DockerVersion: Docker version result" {
 		t.Fatal("Wrong docker version")
 	}
 	if parsed.Query().Get(sendCredentialsURLParameterName) != "true" {
